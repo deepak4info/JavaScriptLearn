4cf1bf5eed827e73c39d7158c44aa96acfd71dfa
Deepak <deepakw2d@gmail.com>
2023-07-27 17:02:38 +0000

    Datatype

---
+++ Basics/Symbol.js
@@ -0,0 +1,9 @@
+const smb1 = Symbol("Hello Deepak");
+const smb2 = Symbol("Hello Deepak");
+
+console.log(smb1 === smb2); // it false 
+
+const str1= "Hello Deepak";
+const str2= "Hello Deepak";
+
+console.log(str1 === str2); // it true 
